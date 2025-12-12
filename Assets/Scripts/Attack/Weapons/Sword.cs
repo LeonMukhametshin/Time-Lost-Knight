de9bd798ec29f954165be5d9bcd6994c5ec90809
Leon <leonmukhametshin2004@gmail.com>
2025-12-12 14:59:09 +0300

    Fix bugs

--- Assets/Scripts/Attack/Weapons/Sword.cs
+++ Assets/Scripts/Attack/Weapons/Sword.cs
@@ -3,9 +3,9 @@ namespace Attacks
     public class Sword : Weapon
     {
         private MeleeWeaponConfig m_config;
-        private IDamageCalculator<WeaponConfig> m_damageCalculator;
+        private IDamageCalculator m_damageCalculator;
 
-        public Sword(MeleeWeaponConfig config, DamageType damageType, IDamageCalculator<WeaponConfig> damageCalculator) : base(config, damageType)
+        public Sword(MeleeWeaponConfig config, DamageType damageType, IDamageCalculator damageCalculator) : base(config, damageType)
         {
             m_config = config;
             m_damageCalculator = damageCalculator;
