3204e6e4449f07d8930f3ecb103f7a40cd3bd95e
LeonMukhametshin <leonmukhametshin2004@gmail.com>
2025-12-14 11:22:23 +0300

    Add Hit method

--- Assets/Scripts/Attack/DamageableWall.cs
+++ Assets/Scripts/Attack/DamageableWall.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 
-public class DamageableWall : ICanBeDamageable
+public class DamageableWall : MonoBehaviour, ICanBeDamageable
 {
-    public int Health { get; private set; }
-
-    public DamageableWall(int health)
-    { 
-        Health = health; 
-    }
+    [field: SerializeField] public int Health { get; private set; } = 100;
 
     public void TakeDamage(DamageType type, int damage)
     {
         Health -= damage;
-        Debug.Log($"TakeDamage with damage: {damage}, damage type: {type}");
+        Debug.Log($"TakeDamage with damage: {damage}, damage type: {type}, current health: {Health}");
+
+        if(Health < 0)
+        {
+            Health = 0;
+            Deastory();
+        }
+    }
+
+    private void Deastory()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Attack/WeaponSystemTests.cs
+++ Assets/Scripts/Attack/WeaponSystemTests.cs
@@ -1,37 +1,60 @@
 using Attacks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeaponSystemTests : MonoBehaviour
 {
     #region OldTest
-    [SerializeField] private int m_wallHealth;
     [SerializeField] private WeaponConfig m_weaponConfig;
     [SerializeField] private CritWeaponConfig m_critConfig;
 
+    [SerializeField] private Transform m_sideAttackTransform;
+    [SerializeField] private Vector2 m_sideAttackArea;
+    [SerializeField] private LayerMask m_damageableLayer;
+
     private IWeapon weapon;
-    private ICanBeDamageable m_damageable;
     private IDamageCalculator m_damageCalculator;
 
     private void Awake()
     {
         weapon = new Weapon(m_weaponConfig, DamageType.Physical);
-        m_damageable = new DamageableWall(m_wallHealth);
-
         m_damageCalculator = new CalculateCritDamage();
     }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Keyboard.current.eKey.wasPressedThisFrame)
         {
-            weapon.ApplyDamage(m_damageable);
+            Debug.Log("Hit");
+            Hit(m_sideAttackTransform, m_sideAttackArea);
         }
-        if(Input.GetMouseButtonDown(1))
+        if(Keyboard.current.qKey.wasPressedThisFrame)
         {
-            Debug.Log(m_damageCalculator);
             weapon = new CritDamageDecorator(weapon, m_damageCalculator, m_critConfig);
             Debug.Log("Update to CriticalDamageDecorator");
         }
+    }
+
+    private void Hit(Transform attackTransform, Vector2 attackArea)
+    {
+        Collider2D[] objectsToHit = Physics2D.OverlapBoxAll(attackTransform.position, attackArea, 0f, m_damageableLayer);
+        
+        foreach(var objectToHit in objectsToHit)
+        {
+            Debug.Log(objectToHit.name);
+            if (objectToHit.TryGetComponent(out ICanBeDamageable damageable))
+            {
+                Debug.Log("damageable     " + objectToHit.name);
+                weapon.ApplyDamage(damageable);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(m_sideAttackTransform.position, m_sideAttackArea);
     }
     #endregion
 }
