04c4049a1f400e7e2d9446a05191da38f3b178ce
OhMyGod <e.yurchenko@xlab-game.com>
2025-12-18 18:08:13 +0300

    Update EnemyData.cs

--- Assets/Scripts/Enemy/EnemyData.cs
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -15,15 +15,14 @@ public sealed class EnemyData : ScriptableObject
     [field: SerializeField] public float m_findingPathTimeMax { get; private set; }
 
     [Header("Ground Check Settings")]
-    [field: SerializeField] public float groundCheckDistance { get; private set; } //Длина луча для проверки земли под врагом
-    [field: SerializeField] public float obstacleCheckDistance { get; private set; } // Длина луча для проверки препятствий перед врагом
+    [field: SerializeField] public float groundCheckDistance { get; private set; } 
+    [field: SerializeField] public float obstacleCheckDistance { get; private set; } 
     [field: SerializeField] public LayerMask groundLayer { get; private set; }
     [field: SerializeField] public LayerMask obstacleLayer { get; private set; }
 
     [Header("Combat Settings")]
     [field: SerializeField] public int m_damage { get; private set; }
     [field: SerializeField] public float m_attackCooldown { get; private set; }
-    [field: SerializeField] public float m_attackRange { get; private set; } // 2
-    //[field: SerializeField] public float detectionRange { get; private set; }
-    [field: SerializeField] public float m_colliderDistanceMultiplier { get; private set; } //-0.25
+    [field: SerializeField] public float m_attackRange { get; private set; } 
+    [field: SerializeField] public float m_colliderDistanceMultiplier { get; private set; } 
 }
