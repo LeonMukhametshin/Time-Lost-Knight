e20323e6eb9a2404b493e456d5cf2eee95325d89
LeonMukhametshin <leonmukhametshin2004@gmail.com>
2026-01-18 02:55:27 +0300

    Fix a bug with getting stuck in a wall during a jump

--- Assets/_Time-Lost-Knight/_Scripts/Players/ScriptableObject/PlayerMovementData.cs
+++ Assets/_Time-Lost-Knight/_Scripts/Players/ScriptableObject/PlayerMovementData.cs
@@ -6,6 +6,5 @@ public sealed class PlayerMovementData : ScriptableObject
     [field: SerializeField] public PlayerMoveData m_moveData { get; private set; }
     [field: SerializeField] public PlayerJumpData m_jumpData { get; private set; }
     [field: SerializeField] public PlayerDashData m_dashData { get; private set; }
-    [field: SerializeField] public PlayerPhysicsData m_physicsData { get; private set; }
     [field: SerializeField] public PlayerGroundCheckData m_groundCheckData { get; private set; }
 }
