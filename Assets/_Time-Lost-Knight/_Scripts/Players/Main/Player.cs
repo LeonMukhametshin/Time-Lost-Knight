6c87b7f62c4d62034ebe699f7934f152e7b6c41d
LeonMukhametshin <leonmukhametshin2004@gmail.com>
2026-01-21 19:40:54 +0300

    Some changes
    Test version

--- Assets/_Time-Lost-Knight/_Scripts/Players/Main/Player.cs
+++ Assets/_Time-Lost-Knight/_Scripts/Players/Main/Player.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IPhysics
 {
+    [SerializeField] private Rigidbody2D m_rigidbody;
+
     [SerializeField] private PlayerData m_data;
     [SerializeField] private PlayerMovementController m_movementController;
     [SerializeField] private AttackInputHandler m_attackInput;
@@ -26,4 +28,7 @@ public class Player : MonoBehaviour
 
         m_healthSystem.valueChanged += () => Debug.Log(m_healthSystem.value);
     }
+
+    public void AddForce(Vector2 direction, ForceMode2D mode) =>
+         m_rigidbody.AddForce(direction, mode);
 }
