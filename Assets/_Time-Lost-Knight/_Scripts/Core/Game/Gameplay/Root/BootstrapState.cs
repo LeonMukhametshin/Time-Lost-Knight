9e88b4223bb2f54686045525d9e99ad340decf8a
prlps <modin.m30@yandex.ru>
2026-03-08 17:56:05 +0300

    Move player camera binding to bootstrap

--- Assets/_Time-Lost-Knight/_Scripts/Core/Game/Gameplay/Root/BootstrapState.cs
+++ Assets/_Time-Lost-Knight/_Scripts/Core/Game/Gameplay/Root/BootstrapState.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -38,7 +39,13 @@ public class BootstrapState : MonoBehaviour, IState
             .completed += _ =>
             {
                 var levelSpawner = ResolveLevelPlayerSpawner();
-                levelSpawner?.Spawn();
+                if(levelSpawner == null)
+                {
+                    return;
+                }
+
+                levelSpawner.Spawn();
+                InitializeLevelCamera();
                 m_healthBarView?.Initialize();
             };
     }
@@ -58,5 +65,39 @@ public class BootstrapState : MonoBehaviour, IState
         }
 
         return m_playerSpawner;
+    }
+
+    private void InitializeLevelCamera()
+    {
+        var virtualCamera = ResolveLevelCamera();
+        if(virtualCamera == null)
+        {
+            return;
+        }
+
+        var player = ServiceLocator.Get<Player>().transform;
+        virtualCamera.Follow = player;
+
+        if(virtualCamera.LookAt == null)
+        {
+            virtualCamera.LookAt = player;
+        }
+    }
+
+    private CinemachineCamera ResolveLevelCamera()
+    {
+        var virtualCameras = FindObjectsByType<CinemachineCamera>(
+            FindObjectsInactive.Exclude,
+            FindObjectsSortMode.None);
+
+        foreach(var virtualCamera in virtualCameras)
+        {
+            if(virtualCamera.gameObject.scene.name == SceneNames.LEVEL_1)
+            {
+                return virtualCamera;
+            }
+        }
+
+        return null;
     }
 }
--- Assets/_Time-Lost-Knight/_Scripts/Core/Infrastructure/States/PlayerSpawner.cs
+++ Assets/_Time-Lost-Knight/_Scripts/Core/Infrastructure/States/PlayerSpawner.cs
@@ -1,4 +1,3 @@
-using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
@@ -17,30 +16,7 @@ public class PlayerSpawner : MonoBehaviour
 
         var player = GameObject.Instantiate(m_player, m_spawnPoint.position, Quaternion.identity, null);
         ServiceLocator.Register(player);
-        BindCinemachine(player);
 
         isPlayerSpawned = true;
-    }
-
-    private void BindCinemachine(Transform player)
-    {
-        if (player == null)
-        {
-            return;
-        }
-
-        var cameras = Object.FindObjectsByType<CinemachineCamera>(
-            FindObjectsInactive.Exclude,
-            FindObjectsSortMode.None);
-
-        if (cameras.Length == 0)
-        {
-            return;
-        }
-
-        var virtualCamera = cameras[0];
-        virtualCamera.Follow = transform;
-        if (virtualCamera.LookAt == null)
-            virtualCamera.LookAt = player;
     }
 }
