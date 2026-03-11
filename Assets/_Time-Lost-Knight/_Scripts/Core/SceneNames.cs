5b5ecb2afcbe33e8d139ee2542a17fe8a9814e4f
Egor <e.yurchenko@xlab-game.com>
2026-03-11 15:09:57 +0300

    First location complete

--- Assets/_Time-Lost-Knight/_Scripts/Core/SceneNames.cs
+++ Assets/_Time-Lost-Knight/_Scripts/Core/SceneNames.cs
@@ -4,5 +4,5 @@ public class SceneNames
     public const string MAIN_MENU= "MainMenu";
     public const string GAMEPLAY = "Gameplay";
     public const string LEVEL_1 = "Level_1";
-    public const string LEVEL_EXAMPLE = "Leon Tests";
+    public const string LEVEL_EXAMPLE = "Egor Level";
 }
