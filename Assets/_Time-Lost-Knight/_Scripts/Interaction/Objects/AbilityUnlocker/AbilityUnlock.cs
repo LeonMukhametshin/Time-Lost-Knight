2188ffcfc4af4f17c84ac7b76fa81ce2df8e7b99
prlps <modin.m30@yandex.ru>
2026-03-17 12:52:35 +0300

    AbilityUnlock sound

--- Assets/_Time-Lost-Knight/_Scripts/Interaction/Objects/AbilityUnlocker/AbilityUnlock.cs
+++ Assets/_Time-Lost-Knight/_Scripts/Interaction/Objects/AbilityUnlocker/AbilityUnlock.cs
@@ -9,6 +9,9 @@ public class AbilityUnlock : MonoBehaviour
     [SerializeField] private AudioClip m_unlockClip;
     [SerializeField][Range(0f, 1f)] private float m_unlockVolume = 1f;
 
+    private void Awake() =>
+        EnsureAudioSource();
+
     private void Start()
     {
         m_playerFSM = ServiceLocator.Get<PlayerFSM>();
@@ -26,4 +29,26 @@ public class AbilityUnlock : MonoBehaviour
         DetachedAudioPlayer.Play(m_audioSource, m_unlockClip, m_unlockVolume);
 
     public virtual void Unlock() { }
+
+    private void EnsureAudioSource()
+    {
+        if (m_audioSource != null)
+        {
+            return;
+        }
+
+        if (!TryGetComponent(out m_audioSource))
+        {
+            m_audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        m_audioSource.playOnAwake = false;
+        m_audioSource.loop = false;
+        m_audioSource.priority = 160;
+        m_audioSource.dopplerLevel = 0f;
+        m_audioSource.spatialBlend = 1f;
+        m_audioSource.minDistance = 2f;
+        m_audioSource.maxDistance = 18f;
+        m_audioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+    }
 }
